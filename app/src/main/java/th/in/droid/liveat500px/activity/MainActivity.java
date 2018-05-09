697cb63408608faf0b9f50f05722a720f547d659
Seven Seconds <sevenseconds@gmail.com>
2018-05-09 22:03:48 +0700

    Implement Drawer Menu

--- app/src/main/java/th/in/droid/liveat500px/activity/MainActivity.java
+++ app/src/main/java/th/in/droid/liveat500px/activity/MainActivity.java
@@ -1,17 +1,27 @@
 package th.in.droid.liveat500px.activity;
 
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import th.in.droid.liveat500px.R;
 import th.in.droid.liveat500px.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initInstances();
 
         if (savedInstanceState == null) {
             getSupportFragmentManager()
@@ -19,5 +29,38 @@ public class MainActivity extends AppCompatActivity {
                     .add(R.id.content_container, MainFragment.newInstance())
                     .commit();
         }
+    }
+
+    private void initInstances() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        drawerToggle = new ActionBarDrawerToggle(
+                MainActivity.this,
+                drawerLayout,
+                R.string.open_menu,
+                R.string.close_menu
+        );
+        drawerLayout.addDrawerListener(drawerToggle);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
